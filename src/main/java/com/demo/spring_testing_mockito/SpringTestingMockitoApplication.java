9456c8d743b1ad53c0956feeda72b2554bcdecc0
NicolasPozzer <pozzernico@gmail.com>
2023-12-12 21:02:49 -0300

    publish in git

---
+++ src/main/java/com/demo/spring_testing_mockito/SpringTestingMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_testing_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestingMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring_testing_mockito/SpringTestingMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_testing_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
